1a5d39d41b06b387fac4f00c835f07b3d45e2561
fcying <fcyingmk2@gmail.com>
2020-07-25 12:49:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+fork [https://github.com/CCChieh/IBMYes](https://github.com/CCChieh/IBMYes)  
+
+
+# 更新内容:
+* `Secrets` 加入 `V2_ID`, `V2_PATH`, 对应`vmess id`和`wsSettings path`
+* 每周自动更新`v2ray`后重新`push`
+
+
+# 设置
+参考  [IBMYES](https://github.com/CCChieh/IBMYes) 进行设置
---
+++ deploy.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+cd $(dirname $0)
+
+IBMCLOUD=$(pwd)/Bluemix_CLI/bin/ibmcloud
+
+if [ ! -f "$IBMCLOUD" ]; then
+    ver=$(curl -s https://github.com/IBM-Cloud/ibm-cloud-cli-release/releases/latest | grep -Po "(\d+\.){2}\d+")
+    wget -q -Oibm_cli.tgz https://clis.cloud.ibm.com/download/bluemix-cli/$ver/linux64
+    if [ $? -eq 0 ]; then
+        tar xzf ibm_cli.tgz
+    else
+        echo "download new version failed!"
+        exit 1
+    fi
+    rm -fv ibm_cli.tgz
+fi
+
+if [ ! -f "./v2ray-cloudfoundry/v2ray/v2ray" ]; then
+    pushd ./v2ray-cloudfoundry/v2ray
+    new_ver=$(curl -s https://github.com/v2fly/v2ray-core/releases/latest | grep -Po "(\d+\.){2}\d+")
+    wget -q -Ov2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v${new_ver}/v2ray-linux-64.zip
+    if [ $? -eq 0 ]; then
+        7z x v2ray.zip v2ray v2ctl
+        chmod 700 v2ctl v2ray
+    else
+        echo "download new version failed!"
+        exit 1
+    fi
+    rm -fv v2ray.zip
+    sed "s/V2_ID/$V2_ID/" config.json -i
+    sed "s/V2_PATH/$V2_PATH/" config.json -i
+    sed "s/IBM_APP_NAME/$IBM_APP_NAME/" ../manifest.yml -i
+    popd
+fi
+
+$IBMCLOUD login -r us-south <<EOF
+$IBM_ACCOUNT
+n
+EOF
+
+if [ ! -f "$HOME/.bluemix/cfcli/cf" ]; then
+    $IBMCLOUD cf install
+    $IBMCLOUD target --cf-api 'https://api.us-south.cf.cloud.ibm.com'
+fi
+
+cd ./v2ray-cloudfoundry
+#$IBMCLOUD target -g $RESOURSE_ID
+$IBMCLOUD target --cf
+
+$IBMCLOUD cf push
